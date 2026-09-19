7be0fa6021dd2b57c7af7d44c6af573e300f566d
abhishek07478 <sp2521@srmist.edu.in>
2026-09-19 10:20:02 +0530

    Add extra Session 3 Java programs

---
+++  Student.java
@@ -0,0 +1,36 @@
+class Student {
+   
+    String name;
+    double attendance;
+
+
+    static String collegeName =
+            "SRM Institute of Science and Technology";
+
+    static int studentCount = 0;
+
+
+    Student(String name, double attendance) {
+        this.name = name;
+        this.attendance = attendance;
+
+        studentCount++;
+    }
+
+    
+    static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Ravi", 85);
+        Student s2 = new Student("Anitha", 90);
+
+        System.out.println(s1.name + " - " + s1.attendance);
+        System.out.println(s2.name + " - " + s2.attendance);
+
+        Student.printCollegeInfo();
+    }
+}
